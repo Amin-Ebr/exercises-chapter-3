86fa2c83ece1b9965ca00b04411d7fd9404ce533
Amin-Ebr <amin.94e@gmail.com>
2025-09-13 13:32:25 +0200

    Exercise 3.4 first attempt

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,4 +1,5 @@
 from numbers import Number
+from numbers import Integral
 
 
 class Polynomial:
@@ -50,3 +51,54 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+    
+    def scalar_mul(self, number):
+        if isinstance(number, Number):
+            return Polynomial( tuple( number* i for i in self.coefficients ) )
+        else:
+            return NotImplemented
+    def __mul__(self,other):
+        if isinstance(other, Polynomial):
+            poly = Polynomial((0,))
+            for i in other.coefficients:
+                poly+= self.scalar_mul(i)
+            return poly
+        elif  isinstance(other, Number):
+            return self.scalar_mul(other)
+        else:
+            return NotImplemented
+    def __rmul__(self, other):
+        return self* other
+    def __sub__(self, other):
+        if isinstance(other,Polynomial ):
+            common= min (self.degree, other.degree)
+            coefs= tuple(a-b for a,b in zip(self.coefficients, other.coefficients))
+            coefs+= self.coefficients(common)+ other.coefficients(common)
+            return Polynomial(coefs)
+        elif isinstance(other, Number):
+            coefs=(self.coefficients[0] - other ,) + self.coefficients[1:]
+            return Polynomial(coefs)
+        else:
+            return NotImplemented
+    def __rsub__(self,other): 
+        return -1 *(self - other)
+    def __pow__(self,int):
+        if isinstance(int , Integral):
+            if int > 0:
+              poly= 1
+              for _ in len(int):
+                poly= poly* self
+              return self
+            else:
+                return NotImplemented
+        else:
+            return NotImplemented
+    def __call__(self, scalar):
+        if isinstance(scalar, Number):
+            value=0
+            for d,c in enumerate(self.coefficients):
+                value+= c*scalar**d
+            return value
+        else:
+            return NotImplemented
+        
