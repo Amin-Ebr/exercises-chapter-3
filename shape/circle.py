a8a92f70c69697cd84c627799f1ec80a567fd20e
Amin-Ebr <amin.94e@gmail.com>
2025-09-14 12:07:29 +0200

    Exercise 3.6 first attempt

---
+++ shape/circle.py
@@ -0,0 +1,10 @@
+class Circle:
+    def __init__(self,centre,rad):
+        self.centre= centre
+        self.radius= rad
+    def __contains__(self, point):
+        if (abs(self.centre[0]-point[0])^2 + abs(self.centre[1]-point[1])^2)< self.radius^2:
+            return True
+        return False 
+        
+   
